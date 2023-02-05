fcdc0a73bf4b0480cedc9c7818062d69d48736f0
Gergely Csatari <gergely.csatari@nokia.com>
2023-02-05 15:04:01 +0200

    [Ri2]: Fixing some of the issues of 3308
    Signed-off-by: Gergely Csatari <gergely.csatari@nokia.com>

--- doc/ref_impl/cntt-ri/conf.py
+++ doc/ref_impl/cntt-ri/conf.py
@@ -24,3 +24,30 @@ autosectionlabel_maxdepth = 4
 numfig = True
 numfig_format = {'figure': 'Figure %s', 'table': 'Table %s',
                  'code-block': 'Listing %s', 'section': 'Section %s'}
+html_static_path = ['_static']
+templates_path = ['_templates']
+html_css_files = [
+    'custom.css',
+]
+
+html_show_sourcelink = False
+html_theme_options = {
+    'nav_title': '',
+     # Set the color and the accent color
+    'color_primary': 'blue-grey,',
+    'color_accent': 'white',
+    # Visible levels of the global TOC; -1 means unlimited
+    'globaltoc_depth': 0,
+    # If False, expand all TOC entries
+    'globaltoc_collapse': False,
+    # If True, show hidden TOC entries
+    'globaltoc_includehidden': False,
+    'base_url': "https://docs.anuket.io/",
+    'repo_url': 'https://gerrit.opnfv.org/',
+    'repo_name': '',
+    'repo_type': 'github',
+}
+
+# Inverse png
+html_logo = '_static/anuket-logo.png'
+html_favicon = '_static/favicon.ico'
